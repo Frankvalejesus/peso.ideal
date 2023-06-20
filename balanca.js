e7ff7143d93a4463ba0f84ac3c91f471aa922d6b
Frankvalejesus <128954966+Frankvalejesus@users.noreply.github.com>
2023-06-20 14:12:31 -0300

    SITE/PESO/IDEAL

---
+++ balanca.js
@@ -0,0 +1,20 @@
+const form = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+   form.addEventListener("submit" , (e) => {
+        e.preventDefault()
+
+        const nome = form.innome.value
+        const masculino = form.inmasculino.checked
+        const altura = Number(form.inaltura.value)
+
+        let peso 
+        if (masculino) { 
+            peso = 22 * Math.pow(altura, 2)
+        }
+
+        else { peso = 21 * Math.pow(altura, 2)
+        }
+
+        resp.innerText = `${nome}: seu peso ideal é ${peso.toFixed(3)} kg`
+    })
